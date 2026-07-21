5586c7d96871460a29372044fad196da065ac50c
Antoine <antoine.frau@kerno.io>
2026-07-21 16:28:43 +0200

    Revert "ci: add HAR harness and endpoint detection factory line (#12)"
    This reverts commit 099ae090eaade045b4f742373d55e5397997349f.

--- .har/factory-line/cases/django-users/files/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from users.views import UserListView
-
-urlpatterns = [
-    path('users/', UserListView.as_view(), name='user-list'),
-]
--- .har/factory-line/cases/next-signup/files/apps/web/app/api/auth/signup/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse, type NextRequest } from "next/server";
-
-async function handler(req: NextRequest) {
-  return NextResponse.json({ ok: true });
-}
-
-export const POST = handler;
